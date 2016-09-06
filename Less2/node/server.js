c28f241c86e0054de8e78cdec5043aaf2aabd4fa
boganatar <boganatar@gmail.com>
2016-09-06 21:39:26 +0300

    less 2

--- Less2/node/server.js
+++ Less2/node/server.js
@@ -3,11 +3,41 @@
  */
 
 var express = require('express');
+var session = require('express-session');
 var app = express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(3000);
 
+
+
+app.use(session({
+    secret: 'angular_tutorial',
+    resave: true,
+    saveUninitialized: true,
+}));
+app.get("/notes", function(req,res) {
+    res.send(req.session.notes||[]);
+});
+
 app.get("/greeting", function(req,res) {
-    res.send("Hello, "+req.query.name+"! I’m server Node JS!");
+    //res.send("Hello, "+req.query.name+"! I’m server Node JS!");
+    req.session.userName = req.query.name;
+    res.send();
+});
+
+app.get("/getname", function(req,res) {
+    res.send(req.session.userName);
+});
+
+app.post("/notes", function(req, res) {
+    if (!req.session.notes) {
+        req.session.notes = [];
+        req.session.last_note_id = 0;
+    }
+    var note = req.body;
+    note.id = req.session.last_note_id;
+    req.session.last_note_id++;
+    req.session.notes.push(note);
+    res.end();
 });

3e75dec9462b790ed4086582031715a0ddf86d0c
boganatar <boganatar@gmail.com>
2016-09-06 21:07:57 +0300

    less 2

--- Less2/node/server.js
+++ Less2/node/server.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(3000);
 
 app.get("/greeting", function(req,res) {
-    res.send("Hello, "+req.query.name+"! I’m server!");
+    res.send("Hello, "+req.query.name+"! I’m server Node JS!");
 });
